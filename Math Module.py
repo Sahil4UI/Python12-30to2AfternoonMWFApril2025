8d95c1dd5a5881e687667bac59c8f7321096f9ac
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-04-25 13:07:10 +0530

    Create Math Module.py

---
+++ Math Module.py
@@ -0,0 +1,21 @@
+Python 3.13.2 (v3.13.2:4f8bb3947cf, Feb  4 2025, 11:51:10) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+import math
+math.cbrt(27)
+3.0
+math.factorial(5)
+120
+5*4*3*2*1
+120
+math.floor(34.546)
+34
+math.ceil(34.546)
+35
+math.ceil(34.125)
+35
+math.fabs(-10)#mod - negative number ko positive banata hai
+10.0
+math.perm(3,2)
+6
+math.comb(3,2)
+3
